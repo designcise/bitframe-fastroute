30af36f12ca68fc73e3335f06815029c76e6ae59
designcise <dev@designcise.com>
2023-05-28 19:43:50 +0200

    test: added RouteTest test

---
+++ test/RouteTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * BitFrame Framework (https://www.bitframephp.com)
+ *
+ * @author    Daniyal Hamid
+ * @copyright Copyright (c) 2017-2023 Daniyal Hamid (https://designcise.com)
+ * @license   https://bitframephp.com/about/license MIT License
+ */
+
+declare(strict_types=1);
+
+namespace BitFrame\FastRoute\Test;
+
+use PHPUnit\Framework\TestCase;
+use BitFrame\FastRoute\Route;
+
+/**
+ * @covers \BitFrame\FastRoute\Route
+ */
+class RouteTest extends TestCase
+{
+    public function routesProvider(): array
+    {
+        return [
+            'Simple GET route' => [
+                ['GET', ''],
+                ['GET', '/'],
+            ],
+            'Multiple routes' => [
+                [['GET', 'POST'], ''],
+                [['GET', 'POST'], '/'],
+            ],
+            'Non-empty route' => [
+                ['PUT', '/foo-bar'],
+                ['PUT', '/foo-bar'],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider routesProvider
+     *
+     * @param array $args
+     * @param array $expected
+     */
+    public function testRouteData(array $args, array $expected): void
+    {
+        $route = new Route(...$args);
+
+        $this->assertSame($expected[0], $route->getMethods());
+        $this->assertSame($expected[1], $route->getPath());
+    }
+}
